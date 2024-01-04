7a27e6b4a344fde257074b5247e334724a72553b
Raihan2503 <lesleyfarm11@gmail.com>
2024-01-04 19:54:55 +0700

    Initalize all files from project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,182 @@
+@extends('layout.layout')
+
+@section('content')
+<div class="jumbotron bg-dark text-white">
+  <div class="position-relative overflow-hidden p-3 p-md-5 text-center">
+    <div class="col-md-6 p-lg-5 mx-auto my-5">
+      <h1 class="display-3 fw-bold">Fiersha Shop</h1>
+      <h3 class="fw-light mb-3">Service is main And Enjoyable</h3>
+      <div class="d-flex gap-3 justify-content-center lead fw-normal">
+        <p class="lead">Fiersha Shop customers is enjoyable</p>
+      </div>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
+</div>
+
+{{-- Main --}}
+<main>
+  {{-- About --}}
+  <div class="album py-5 bg-body-tertiary">
+    <div class="container">
+      <h1 class="text-center my-3">Product</h1>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <h5 class="card-text fw-normal">T-Shirt</h5>
+              <span>Harga: Rp150.000</span><br>
+              <span>Warna: Orange</span>
+              <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="btn-group">
+                  <a type="button" class="btn btn-primary btn-sm me-2">Beli Produk</a>
+                  <a type="button" class="btn btn-warning btn-sm">Keranjang</a>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</main>
+@endsection
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar bg-dark navbar-expand-md border-bottom border-body" data-bs-theme="dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="{{asset('assets/images/logo-fiersha2.png')}}" alt="">
+        Fiersha Shop
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Keranjang</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Produk</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#">Testimoni</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Category</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
